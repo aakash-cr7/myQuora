915ad1152976533a71c562230449b5b109a57975
aakash-cr7 <aakash.tyg93@gmail.com>
2015-11-10 12:44:26 +0530

    Search done

--- myQuora/question/views.py
+++ myQuora/question/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods, require_GET, require_POST
 from django.core.paginator import Paginator
+from django.db.models import Q
 from .forms import QuestionCreateForm
 from .models import Question, Answer, Topic
 
@@ -34,3 +36,16 @@ def myquestions(request, page_num = 1):
     }
     return render(request, 'questions/myquestions.html', context)
 
+@login_required
+@require_GET
+def search(request):
+    query_term = request.GET.get('q')
+    data = {'question': []}
+    if not query_term:
+        return JsonResponse(data)
+    questions = Question.objects.filter(
+        Q(title__icontains = query_term) | Q(desc__icontains = query_term)
+    )
+    data['question'] = [ {'id': q.id, 'title': q.title} for q in questions ]
+    print(data)
+    return JsonResponse(data)
